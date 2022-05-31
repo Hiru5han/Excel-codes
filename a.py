ec2023f23f38f8b7deebac7aa65b76e6538734ae
Hiru5han <88285901+Hiru5han@users.noreply.github.com>
2022-05-31 06:06:01 +0100

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,3 @@
-for idx in range(1, 2001):
-    x='=INDIRECT('
-    y='"Form1!"'
-    eq = f',&ADDRESS(ROW(${idx}:${idx}),COLUMN($C:$C)))'
-    print(x+y+eq)
+for idx in range(1, 16381):
+    eq = f'=INDIRECT("Form1!",&ADDRESS(ROW(${idx}:${idx}),COLUMN($C:$C)))'
+    print(eq)
